47b71cd0f8bd17b4073603ac9ea319059e36d9ec
Serafeim Papastefanos <spapas@gmail.com>
2016-07-05 00:52:49 +0300

    Add django-debug-toolbar

--- django_table_filtering/settings.py
+++ django_table_filtering/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
     'django_extensions',
     'django_filters',
     'django_tables2',
