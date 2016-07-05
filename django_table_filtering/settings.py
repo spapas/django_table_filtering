8f5176d033d744bb2f1347292b2d872b3d342866
Serafeim Papastefanos <spapas@gmail.com>
2016-07-05 08:19:36 +0300

    Use postgresql db

--- django_table_filtering/settings.py
+++ django_table_filtering/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'django_table_filtering.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'table_filtering',                      
+        'USER': 'serafeim',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
